7416a43dc2cc73eb86e45460ac07ce00a4106bcb
Sebastien Lefebvre - Kitpages <sebastien.lefebvre@kitpages.fr>
2012-10-10 15:53:13 +0200

    form migration

--- Form/RedirectionType.php
+++ Form/RedirectionType.php
@@ -3,11 +3,11 @@
 namespace Kitpages\RedirectBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class RedirectionType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('sourceUrl')
